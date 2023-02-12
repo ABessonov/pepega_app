6a4aef264d6141fdc4ce786e9cdaa604ca3b1871
Aleksandr Bessonov <bessonov@soyuz317.ru>
2023-02-12 13:23:13 +0700

    sdsds

--- pepega_app/frontend/tailwind.config.js
+++ pepega_app/frontend/tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["*"],
   theme: {
     extend: {},
   },
